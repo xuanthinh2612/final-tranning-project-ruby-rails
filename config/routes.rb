c6d2ddc98f8a67abcaf7bb858b5bc6a59e17a196
Thinh Tran Xuan <you@example.com>
2021-06-14 14:05:04 +0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,5 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get 'chatroom/index'
-  mount ActionCable.server, at: '/cable'
+  # mount ActionCable.server, at: '/cable'
 end
